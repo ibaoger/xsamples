66433317a0315537997f8446d17297485add67a8
shishuo <shishuo365@gmail.com>
2015-09-17 18:29:20 +0800

    增加未完成的TCP客户端程序

--- tcp/c/client.c
+++ tcp/c/client.c
@@ -1,5 +1,19 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+
+int serverPort = 32015;
 
 int main(int argc, char **argv)
 {
-	return 0;
+    int err;
+    int sock;
+    struct sockaddr_in serveraddr;
+
+    /* get a socket descriptor */
+    int domain;
+    int type;
+    int protocol;
+    err = socket(AF_INET, SOCK_STREAM, int protocol);
+
+    return 0;
 }
--- tcp/c/server.c
+++ tcp/c/server.c
@@ -1,5 +1,10 @@
 
 int main(int argc, char **argv)
 {
+    int sock;
+    struct sockaddr_in serveraddr;
+
+    /* get a socket descriptor */
+
 	return 0;
 }
